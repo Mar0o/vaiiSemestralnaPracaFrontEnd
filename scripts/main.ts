8c75c9070a529bbff4268224db55ed92e3292080
Maroo <maros372@gmail.com>
2024-10-20 20:47:05 +0200

    final checkpoint update

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const navLinks = document.querySelectorAll("nav a");
-
     navLinks.forEach((link) => {
       link.addEventListener("click", (event) => {
         navLinks.forEach((nav) => nav.classList.remove("active"));
